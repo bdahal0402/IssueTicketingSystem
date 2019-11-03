0458d11ba887a99cc4c7fbe5ed24ba2ddc4b8d7a
Zachary Burton <45068978+burtonzr@users.noreply.github.com>
2019-11-03 14:17:01 -0600

    Alert box added for wrong login combination.

--- src/Login.java
+++ src/Login.java
@@ -14,101 +14,74 @@ import javax.servlet.http.HttpSession;
 
 import java.sql.*;
 
-/**
- * Servlet implementation class Login
- */
+// Servlet implementation class Login
+@SuppressWarnings("serial")
 @WebServlet("/Login")
-public class Login extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-    /**
-     * Default constructor. 
-     */
-    public Login() {
-        // TODO Auto-generated constructor stub
-    }
-
+public class Login extends HttpServlet implements SetConnection {
+	@SuppressWarnings("unused")
+	private static final long serialIDVersion = 1L;
+	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.sendRedirect("login.jsp");
 	}
-
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
 		doGet(request, response);
 		
 		String name = request.getParameter("uname");
 		String pass = request.getParameter("upass");
 		
-		System.out.println("Name =" + name + "Pass " + pass);
+		//System.out.println("Name =" + name + "Pass " + pass);
 		
-	
+		Connection con = null;
+		try {
+			try {
+				Class.forName("org.postgresql.Driver");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
 			
-				Connection con = null;
-				try {
-					
-					
-					try {
-						Class.forName("org.postgresql.Driver");
-					} catch (ClassNotFoundException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					
-					String url = "jdbc:postgresql://ec2-54-235-246-201.compute-1.amazonaws.com/d712a16gfjlf2i";
-			        String username = "qpvmvoqkxifbdv";
-			        String password = "7bb011180f5880de08fe6c69f68647a5a8409ccc13528729b792dcdee7df9512";
-			        
-			        con = DriverManager.getConnection(url,username,password);
-		            System.out.println("DB connected successfully!");
-					
-					
-					
-					Statement stmt = con.createStatement();
-					
-					
-					
-//					ResultSet rs = stmt.executeQuery("select * from users");
-//					   ResultSetMetaData rsmd = rs.getMetaData();
-//					   int columnsNumber = rsmd.getColumnCount();
-//					   while (rs.next()) {
-//					       for (int i = 1; i <= columnsNumber; i++) {
-//					           if (i > 1) System.out.print(",  ");
-//					           String columnValue = rs.getString(i);
-//					           System.out.print(columnValue + " " + rsmd.getColumnName(i));
-//					       }
-//					       System.out.println("");
-//					   }
-					
-					
-					
-					//ResultSet rs = stmt.executeQuery("select * from user_reg");
-					ResultSet rs2 = stmt.executeQuery("select firstname,userpassword from users where firstname = '"+name+"'and userpassword = '"+AES.encrypt(pass, "passwordEncryption")+"'   ");
+			con = DriverManager.getConnection(url, username, password);
+			//System.out.println("DB connected successfully!");
 			
-					
-					
-					if(rs2.next()) {
-						HttpSession session = request.getSession();
-						session.setAttribute("uname", name);
-						response.sendRedirect("WelcomeUser.jsp?uname="+name+"");
-					}
-					else {
-						out.println("Wrong username or password");
-					}
-					
-					
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-		
+			Statement stmt = con.createStatement();
+			
+			ResultSet rs2 = stmt.executeQuery("SELECT Username, UserPassword FROM Users WHERE Username = '"+name+"' AND UserPassword = '"+AES.encrypt(pass, "passwordEncryption")+"'   ");
+			
+			if(rs2.next()) {
+				HttpSession session = request.getSession();
+				session.setAttribute("uname", name);
+				response.sendRedirect("WelcomeUser.jsp?uname=" + name);
+			} else {
+				response.setContentType("text/html");
+				out.println("<script type=\"text/javascript\">");
+				out.println("alert('Wrong username and password combination.');");
+				out.println("</script>");
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
-
+	
+	public static boolean checkDBusername(String Username) {
+		DBConnection db = new DBConnection();
+		String query = String.format("SELECT COUNT(*) FROM Users WHERE Username='%s'", Username);
+		db.runQuery(query);
+		return true;
+	}
+	
+	public static boolean checkDBPassword(String Password) {
+		DBConnection db = new DBConnection();
+		String query = String.format("SELECT COUNT(*) FROM Users WHERE UserPassword='%s'", Password);
+		db.runQuery(query);
+		return true;
+	}
 }
