61d028f6698672a5cadcf771b15d1ee0cbea161a
Zachary Burton <45068978+burtonzr@users.noreply.github.com>
2019-11-23 15:28:31 -0600

    Create UpdateTicket.java

---
+++ src/UpdateTicket.java
@@ -0,0 +1,73 @@
+package TicketPackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/UpdateTicket")
+public class UpdateTicket extends HttpServlet implements SetConnection {
+	@SuppressWarnings("unused")
+	private static final long serialIDVersion = 1;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		@SuppressWarnings("unused")
+		PrintWriter out = response.getWriter();
+		doGet(request, response);
+		
+		String ticketid = request.getParameter("ticketid");
+		String status   = request.getParameter("status");
+		
+		Connection con = null;
+		
+		try {
+			try {
+				Class.forName("org.postgresql.Driver");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			
+			con = DriverManager.getConnection(url, username, password);
+			
+			Statement stmt = con.createStatement();
+			
+			String values = String.format("'%s', '%s'", ticketid, status);
+			
+			System.out.println(values);
+			
+			int rows = stmt.executeUpdate("UPDATE tickets SET status = '1' WHERE id = " + ticketid);
+			
+			if(rows == 1) {
+				response.setContentType("text/html");
+				out.println("<script type=\"text/javascript\">");
+				out.println("alert('The ticket has been completed. ')");
+				out.println("location='Assigned.jsp';");
+				out.println("</script>");
+			} else {
+				response.setContentType("text/html");
+				out.println("<script type=\"text/javascript\">");
+				out.println("alert('The ticket could not be updated. ')");
+				out.println("location='Assigned.jsp';");
+				out.println("</script>");
+			}
+			
+			con.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
